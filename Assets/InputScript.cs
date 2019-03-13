ee1b447bc83021477b2ab1d5ca533935c8cc4780
Mark <c16765279@mydit.ie>
2019-03-13 18:54:21 +0000

    Updated UI

--- Assets/InputScript.cs
+++ Assets/InputScript.cs
@@ -9,7 +9,8 @@ public class InputScript : MonoBehaviour
     public KeyCode Button1, Button2;
     public KeyCode[] Keys;
     private int actionState;
-    public string actionToDisplay, buttonPressString;
+    private string actionToDisplay, buttonPressString, buttonString1, buttonString2;
+    private int buttonNumber;
     public int health = 100;
     private float timePerAction;
     public float maxTimePerAction;
@@ -85,11 +86,14 @@ public class InputScript : MonoBehaviour
             maxTimePerAction -= timeIncrement;
         }
 
+        ChangeText();
+        
+
     }
     
     void reshuffle(KeyCode[] Keys)
     {
-        // Knuth shuffle algorithm :: courtesy of Wikipedia :)
+        
         for (int t = 0; t < Keys.Length; t++ )
         {
             KeyCode tmp = Keys[t];
@@ -99,11 +103,96 @@ public class InputScript : MonoBehaviour
         }
     }
 
+    void ChangeText()
+    {
+        if (Button1 == blueSquare)
+        {
+            buttonString1 = "Blue Square";
+        }
+        
+        if (Button1 == blueCircle)
+        {
+            buttonString1 = "Blue Circle";
+        }
+        
+        if (Button1 == blueTriangle)
+        {
+            buttonString1 = "Blue Triangle";
+        }
+        
+        if (Button1 == blueStar)
+        {
+            buttonString1 = "Blue Star";
+        }
+        
+        if (Button1 == redSquare)
+        {
+            buttonString1 = "Red Square";
+        }
+        
+        if (Button1 == redCircle)
+        {
+            buttonString1 = "Red Circle";
+        }
+        
+        if (Button1 == redTriangle)
+        {
+            buttonString1 = "Red Triangle";
+        }
+        
+        if (Button1 == redStar)
+        {
+            buttonString1 = "Red Star";
+        }
+        
+        
+        if (Button2 == blueSquare)
+        {
+            buttonString2 = "Blue Square";
+        }
+        
+        if (Button2 == blueCircle)
+        {
+            buttonString2 = "Blue Circle";
+        }
+        
+        if (Button2 == blueTriangle)
+        {
+            buttonString2 = "Blue Triangle";
+        }
+        
+        if (Button2 == blueStar)
+        {
+            buttonString2 = "Blue Star";
+        }
+        
+        if (Button2 == redSquare)
+        {
+            buttonString2 = "Red Square";
+        }
+        
+        if (Button2 == redCircle)
+        {
+            buttonString2 = "Red Circle";
+        }
+        
+        if (Button2 == redTriangle)
+        {
+            buttonString2 = "Red Triangle";
+        }
+        
+        if (Button2 == redStar)
+        {
+            buttonString2 = "Red Star";
+        }
+    }
+
     private void OnGUI()
     {
         GUI.Label(new Rect(10, 10, 1000, 20), actionToDisplay + " " + buttonPressString);
         GUI.Label(new Rect(10, 30, 1000, 20), ""+ timePerAction);
         GUI.Label(new Rect(10, 50, 1000, 20), "Health: "+ health);
         GUI.Label(new Rect(10, 70, 1000, 20), "Score: "+ score);
+        GUI.Label(new Rect(10, 90, 1000, 20), buttonString1 + " " + buttonString2);
     }
 }
